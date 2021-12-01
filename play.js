a12b50076158bca757e80123b9a50a696868377f
joshuavottero <joshuavottero@outlook.com>
2021-12-01 20:31:03 +0000

    added input module for play.js containg the setupInput function

--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 const {connect} = require("./client");
+const {setupInput} = require("./input");
 //const net = require("net");
 
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function(key){
-    if (key === "\u0003"){
-      process.exit();
-    }
-  }
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const net = require("net");
-
 
 setupInput();
 
